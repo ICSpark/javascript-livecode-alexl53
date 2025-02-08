0977c0ec812aafa3cf1427ad9b800201f3e506fa
alexl53 <alexander88lee@gmail.com>
2025-02-08 19:44:39 +0000

    added checks to see if somebody is in debt

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello world!")
+
+var dolla = 70;
+var debt = 600;
+
+if(dolla >= 70) {
+    console.log("good morning");
+}
+if(debt >= 500) {
+    console.log("you're in debt to the cartel")
+}
